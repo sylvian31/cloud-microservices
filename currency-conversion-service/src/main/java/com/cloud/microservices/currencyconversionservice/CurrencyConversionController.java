599155332892174197cc04ff649c05b994280580
sylvian <sylvian31@gmail.com>
2019-04-16 19:50:02 -0400

    mis en place spring cloud sleuth

--- currency-conversion-service/src/main/java/com/cloud/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/cloud/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,17 +4,20 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-	
+
+	Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+
 	@Autowired
 	private CurrencyExchangeServiceProxy CurrencyExchangeServiceProxy;
 
@@ -27,21 +30,23 @@ public class CurrencyConversionController {
 		uriVariables.put("to", to);
 
 		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
-				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
+				uriVariables);
 
 		CurrencyConversionBean response = responseEntity.getBody();
 		return new CurrencyConversionBean(response.getId(), from, to, quantity, response.getConvertionMultiple(),
 				quantity.multiply(response.getConvertionMultiple()), response.getPort());
 
 	}
-	
-	
+
 	@GetMapping(path = "/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
-		
+
 		CurrencyConversionBean response = CurrencyExchangeServiceProxy.getExhangeValue(from, to);
-		
+
+		logger.info("{}", response);
+
 		return new CurrencyConversionBean(response.getId(), from, to, quantity, response.getConvertionMultiple(),
 				quantity.multiply(response.getConvertionMultiple()), response.getPort());
 
--- currency-exchange-service/src/main/java/com/cloud/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/cloud/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.cloud.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Repository;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/currency-exchange")
 public class CurrencyExchangeController {
 	
+	Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private Environment environment;
 	
@@ -24,6 +28,7 @@ public class CurrencyExchangeController {
 	public ExchangeValue getExhangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("{}", exchangeValue);
 		return exchangeValue;
 	}
 
